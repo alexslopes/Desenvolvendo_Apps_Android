b39c2ea02c6eae3ad42424fc0e9afd3f2a9975a2
Alex <alexstlopes@gmail.com>
2018-01-26 12:51:49 -0300

    A02.22

--- Shushine/app/src/main/java/com/example/alex/shushine/MainActivity.java
+++ Shushine/app/src/main/java/com/example/alex/shushine/MainActivity.java
@@ -1,38 +1,72 @@
 package com.example.alex.shushine;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import com.example.alex.shushine.utilities.NetworkUtils;
+import com.example.alex.shushine.utilities.OpenWeatherJsonUtils;
+import com.example.alex.shushine.utilities.SunshinePreferences;
+
+import java.net.URL;
+
+import static com.example.alex.shushine.utilities.OpenWeatherJsonUtils.getSimpleWeatherStringsFromJson;
+
 public class MainActivity extends AppCompatActivity {
 
-    TextView weatherItem;
+    private TextView mWeatherTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        this.weatherItem = (TextView) findViewById(R.id.weather_item);
+        this.mWeatherTextView = (TextView) findViewById(R.id.weather_item);
 
-        String[] dummyWeatherData = {
-                "Today, May 17 - Clear - 17°C / 15°C",
-                                "Tomorrow - Cloudy - 19°C / 15°C",
-                                "Thursday - Rainy- 30°C / 11°C",
-                                "Friday - Thunderstorms - 21°C / 9°C",
-                                "Saturday - Thunderstorms - 16°C / 7°C",
-                                "Sunday - Rainy - 16°C / 8°C",
-                                "Monday - Partly Cloudy - 15°C / 10°C",
-                                "Tue, May 24 - Meatballs - 16°C / 18°C",
-                                "Wed, May 25 - Cloudy - 19°C / 15°C",
-                                "Thu, May 26 - Stormy - 30°C / 11°C",
-                                "Fri, May 27 - Hurricane - 21°C / 9°C",
-                                "Sat, May 28 - Meteors - 16°C / 7°C",
-                                "Sun, May 29 - Apocalypse - 16°C / 8°C",
-                                "Mon, May 30 - Post Apocalypse - 15°C / 10°C",
-        };
+        loadWeatherData();
 
-        for(String dummyWeatherDay : dummyWeatherData ){
-            weatherItem.append(dummyWeatherDay + "\n\n\n");
+    }
+
+    private void loadWeatherData() {
+        String location = SunshinePreferences.getPreferredWeatherLocation(this);
+        new FetchWeatherTask().execute(location);
+    }
+
+    public class FetchWeatherTask extends AsyncTask<String, Void, String[]> {
+        @Override
+        protected String[] doInBackground(String... params) {
+            /* If there's no zip code, there's nothing to look up. */
+            if (params.length == 0) {
+                return null;
+            }
+
+            String location = params[0];
+            URL weatherRequestUrl = NetworkUtils.buildUrl(location);
+
+            try {
+                String jsonWeatherResponse = NetworkUtils
+                        .getResponseFromHttpUrl(weatherRequestUrl);
+
+                String[] simpleJsonWeatherData = OpenWeatherJsonUtils
+                        .getSimpleWeatherStringsFromJson(MainActivity.this, jsonWeatherResponse);
+
+                return simpleJsonWeatherData;
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String[] weatherData){
+            if(weatherData != null){
+                for(String weatherString : weatherData){
+                    mWeatherTextView.append((weatherString) + "\n\n\n");
+                }
+
+            }
+
         }
     }
 }
--- Shushine/app/src/main/java/com/example/alex/shushine/utilities/NetworkUtils.java
+++ Shushine/app/src/main/java/com/example/alex/shushine/utilities/NetworkUtils.java
@@ -1,6 +1,14 @@
 package com.example.alex.shushine.utilities;
 
+import android.net.Uri;
+import android.util.Log;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Scanner;
 
 /**
  * Created by alex.lopes on 25/01/2018.
@@ -33,7 +41,42 @@ public class NetworkUtils {
     final static String DAYS_PARAM = "cnt";
 
     public static URL buildUrl(String locationQuery){
-        
+        Uri builtUri = Uri.parse(FORECAST_BASE_URL).buildUpon()
+                .appendQueryParameter(QUERY_PARAM, locationQuery)
+                .appendQueryParameter(FORMAT_PARAM, format)
+                .appendQueryParameter(UNITS_PARAM, units)
+                .appendQueryParameter(DAYS_PARAM, Integer.toString(numDays))
+                .build();
+
+        URL url = null;
+        try{
+            url = new URL(builtUri.toString());
+        }catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        Log.v(TAG, "Built URI " + url);
+
+        return url;
+    }
+
+    public static String getResponseFromHttpUrl(URL url) throws IOException {
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+        try {
+            InputStream in = urlConnection.getInputStream();
+
+            Scanner scanner = new Scanner(in);
+            scanner.useDelimiter("\\A");
+
+            boolean hasInput = scanner.hasNext();
+            if(hasInput){
+                return  scanner.next();
+            }else {
+                return null;
+            }
+        }finally {
+            urlConnection.disconnect();
+        }
     }
 
 }
