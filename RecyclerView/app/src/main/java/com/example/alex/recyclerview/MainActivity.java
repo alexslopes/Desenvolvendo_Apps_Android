e31ab55e19ef4ebb7f71ffddc7f28e845bfe60a9
Alex <alexstlopes@gmail.com>
2018-02-09 02:48:50 -0300

    03.18

--- RecyclerView/app/src/main/java/com/example/alex/recyclerview/MainActivity.java
+++ RecyclerView/app/src/main/java/com/example/alex/recyclerview/MainActivity.java
@@ -2,12 +2,26 @@ package com.example.alex.recyclerview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static  final int NUM_LIST_ITEMS = 100;
+    private GreenAdapter mAdapter;
+    private RecyclerView mNumbersList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mNumbersList = (RecyclerView) findViewById(R.id.rv_numbers);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        mNumbersList.setLayoutManager(layoutManager);
+
+        mNumbersList.setHasFixedSize(true);
+        mAdapter = new GreenAdapter(NUM_LIST_ITEMS);
+        mNumbersList.setAdapter(mAdapter);
     }
 }
